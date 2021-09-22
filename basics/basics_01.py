6b7f9b3b5d5123e2aa00aaff9a384cd69308fe90
Shegami <kanstantsin.toukach@gmail.com>
2021-09-22 14:46:25 +0300

    basics 01 - 03

---
+++ basics/basics_01.py
@@ -0,0 +1,11 @@
+a = 5
+print(a)
+
+b = 'string'
+print(b)
+
+c = 3.2
+print(c)
+
+d = True
+print(d)
---
+++ basics/basics_03.py
@@ -0,0 +1,15 @@
+my_age = 22
+olya_age = 27
+vanya_age = 25
+
+sum_ages = sum(
+    [
+        my_age,
+        olya_age,
+        vanya_age
+    ]
+)
+print(sum_ages)
+
+avg = (my_age + olya_age + vanya_age) / 3
+print(avg)
